11c15778caeb7ae8fb560fdbb1669b1d49c9c474
Sagar Gurunath Bandkar <sagarbndkr@outlook.com>
2020-03-08 12:15:54 +0530

    BubbleSort.

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+	public static class BubbleSort
+	{
+		public static void Fun_BubbleSort(int[] a)
+		{
+			int i, j, t;
+
+			for (i = a.Length - 2; i >= 0; i--)
+			{
+				for (j=0;j<=i;j++)
+				{
+					if (a[j] > a[j + 1])
+					{
+						t = a[j];
+						a[j] = a[j + 1];
+						a[j + 1] = t;
+					}
+				}
+			}
+		}
+	}
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -10,6 +10,30 @@ namespace Sorting
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("-------------------Sorting Demo Project---------------------");
+			int[] a = { 45, 85, 12, 6, 74, 96, 156, 1000, 862, 456, 2, 1, 89, 302 };
+			Console.WriteLine("Size of an array:- " + a.Count()+"\n");
+			Console.WriteLine("Before Sorting :- ");
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.Write(a[i]);
+				if (i < a.Length - 1)
+				{
+					Console.Write(",");
+				}
+			}
+			Console.WriteLine("\n\nAfter Sorting :- ");
+			BubbleSort.Fun_BubbleSort(a);
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.Write(a[i]);
+				if (i < a.Length - 1)
+				{
+					Console.Write(",");
+				}
+			}
+			Console.WriteLine("\n\n---------------------------s----------------------------------");
+			Console.ReadLine();
 		}
 	}
 }
